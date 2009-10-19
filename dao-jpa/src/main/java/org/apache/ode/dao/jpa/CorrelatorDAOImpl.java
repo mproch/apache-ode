57553254f5eed57c738c64b18890f3f31eff4f3c
Rafał Rusin <rr@touk.pl>
2009-10-19 16:28:10 +0200

    Logging + fix

--- dao-jpa/src/main/java/org/apache/ode/dao/jpa/CorrelatorDAOImpl.java
+++ dao-jpa/src/main/java/org/apache/ode/dao/jpa/CorrelatorDAOImpl.java
@@ -19,6 +19,8 @@
 
 package org.apache.ode.dao.jpa;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.ode.bpel.common.CorrelationKeySet;
 import org.apache.ode.bpel.dao.*;
 
@@ -32,6 +34,7 @@ import java.util.List;
 @Table(name = "ODE_CORRELATOR")
 @NamedQueries( { @NamedQuery(name = CorrelatorDAOImpl.DELETE_CORRELATORS_BY_PROCESS, query = "delete from CorrelatorDAOImpl as c where c._process = :process") })
 public class CorrelatorDAOImpl extends OpenJPADAO implements CorrelatorDAO {
+    private static Log __log = LogFactory.getLog(CorrelatorDAOImpl.class);
     public final static String DELETE_CORRELATORS_BY_PROCESS = "DELETE_CORRELATORS_BY_PROCESS";
     private final static String ROUTE_BY_CKEY_HEADER = "select route from MessageRouteDAOImpl as route where route._correlator._process._processType = :ptype and route._correlator._correlatorKey = :corrkey";
 
@@ -60,8 +63,12 @@ public class CorrelatorDAOImpl extends OpenJPADAO implements CorrelatorDAO {
     }
 
     public void addRoute(String routeGroupId, ProcessInstanceDAO target, int index, CorrelationKeySet correlationKeySet, String routePolicy) {
+        if (__log.isDebugEnabled()) {
+            __log.debug("addRoute " + routeGroupId + " " + target + " " + index + " " + correlationKeySet + " " + routePolicy);
+        }
         MessageRouteDAOImpl mr = new MessageRouteDAOImpl(correlationKeySet, routeGroupId, index, (ProcessInstanceDAOImpl) target, this, routePolicy);
         _routes.add(mr);
+        getEM().flush();
     }
 
     public MessageExchangeDAO dequeueMessage(CorrelationKeySet correlationKeySet) {
@@ -89,6 +96,9 @@ public class CorrelatorDAOImpl extends OpenJPADAO implements CorrelatorDAO {
 
     @SuppressWarnings("unchecked")
     public List<MessageRouteDAO> findRoute(CorrelationKeySet correlationKeySet) {
+        if (__log.isDebugEnabled()) {
+            __log.debug("findRoute " + correlationKeySet);
+        }
         List<CorrelationKeySet> subSets = correlationKeySet.findSubSets();
         Query qry = getEM().createQuery(generateSelectorQuery(ROUTE_BY_CKEY_HEADER, subSets));
         qry.setParameter("ptype", _process.getType().toString());
@@ -112,8 +122,14 @@ public class CorrelatorDAOImpl extends OpenJPADAO implements CorrelatorDAO {
                     routed = true;
                 }
             }
+            if (__log.isDebugEnabled()) {
+                __log.debug("findRoute found " + matchingRoutes);
+            }
             return matchingRoutes;
         } else {
+            if (__log.isDebugEnabled()) {
+                __log.debug("findRoute found nothing");
+            }
             return null;
         }
     }
@@ -151,10 +167,16 @@ public class CorrelatorDAOImpl extends OpenJPADAO implements CorrelatorDAO {
     }
 
     void removeLocalRoutes(String routeGroupId, ProcessInstanceDAO target) {
+        if (__log.isDebugEnabled()) {
+            __log.debug("removeLocalRoutes " + routeGroupId);
+        }
         boolean flush = false;
         for (Iterator<MessageRouteDAOImpl> itr = _routes.iterator(); itr.hasNext();) {
             MessageRouteDAOImpl mr = itr.next();
             if (mr.getGroupId().equals(routeGroupId) && mr.getTargetInstance().equals(target)) {
+                if (__log.isDebugEnabled()) {
+                    __log.debug("removing " + mr.getCorrelationKey() + " " + mr.getIndex() + " " + mr.getRoute());
+                }
                 itr.remove();
                 getEM().remove(mr);
                 flush = true;
